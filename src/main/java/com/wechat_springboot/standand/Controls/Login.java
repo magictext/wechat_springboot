e7b25a56b94b0993fd6b09b6625e791d514d43ee
magictext <743145068@qq.com>
2018-12-04 16:00:53 +0800

    框架搭建

--- src/main/java/com/wechat_springboot/standand/Controls/Login.java
+++ src/main/java/com/wechat_springboot/standand/Controls/Login.java
@@ -3,21 +3,28 @@ package com.wechat_springboot.standand.Controls;
 import com.wechat_springboot.standand.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 
-@Controller
+@RestController
 public class Login {
-    @RequestMapping(value = "/{xiling}",method = RequestMethod.GET)
-    @ResponseBody
-    public Student loginin(@PathVariable("xiling")  String name){
-        Student student =new Student();
-        student.setClass("166");
-        student.setId("16263626");
-        student.setName(name);
+//    @RequestMapping(value = "/{xiling}",method = RequestMethod.GET)
+//    public Student loginin(@PathVariable("xiling")  String name){
+//        Student student =new Student();
+//        student.setClass("166");
+//        student.setId("16263626");
+//        student.setName(name);
+//        return student;
+//    }
+    @RequestMapping(value = "/loginin",method = RequestMethod.GET)
+    public Student loginin(String code){
+        String url="https://api.weixin.qq.com/sns/jscode2session?" +
+                "appid=wx7c3fa8b4de3fa68f&" +
+                "secret=5d50c86aba4cd65a0b1e1a53b5c1a194&" +
+                "js_code="+code+"&" +
+                "grant_type=authorization_code";
+
         return student;
     }
 }
